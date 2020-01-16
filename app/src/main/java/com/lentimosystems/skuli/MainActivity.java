be433321b78a919c115a2d81be2bcad7839ab3f2
liciolentimo <lentimo@gmail.com>
2020-01-16 07:36:26 +0300

    Initial commit

---
+++ app/src/main/java/com/lentimosystems/skuli/MainActivity.java
@@ -0,0 +1,34 @@
+package com.lentimosystems.skuli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<AuthUI.IdpConfig> providers;
+    private static final int MY_REQUEST_CODE = 1111;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        providers = Arrays.asList(new AuthUI.IdpConfig.FacebookBuilder().build());
+
+        showSignInOptions();
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setTheme(R.style.MyTheme)
+                .build(),MY_REQUEST_CODE 
+        );
+    }
+}
---
+++ app/src/main/java/com/lentimosystems/skuli/SuccessLoginActivity.java
@@ -0,0 +1,14 @@
+package com.lentimosystems.skuli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_login);
+    }
+}
